bae768a557bcc0c2ff605340b1e7454590952249
lmpescobar <lmpes@tutanota.com>
2024-08-11 22:15:09 -0400

    Sintaxis, indentación y comentarios

---
+++ c02_s_i_c.md/p01_proyecto.py
@@ -0,0 +1,17 @@
+# Sintaxis, indentación y comentarios
+# indentación
+print("Hola")
+
+if 5 > 3:
+    if 5 > 4:
+        print("5 es mayor que 4 y que 3")
+
+# Esto es un comentario
+print("Esto sale en consola")
+# ¿Esto sale en consola?
+
+"""
+aaaaaaa
+eeeeeee
+Esto es un comentario multilinea
+"""
